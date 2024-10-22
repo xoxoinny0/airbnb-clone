40b5ddd113e2d4a6f1297489cf447c75094a33f9
xoxoinny0 <xoxoinny0@gmail.com>
2024-10-22 17:05:50 +0900

    feat : 프로젝트 생성

---
+++ src/app/_component/Header.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+
+export default function Header() {
+  return (
+    <header>
+      <Button variant="contained">Hello world</Button>
+      header
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Header from "./_component/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <Header />
         {children}
       </body>
     </html>
