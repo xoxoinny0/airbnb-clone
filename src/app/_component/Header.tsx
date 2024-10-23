47a372bfa7a3aac193decbcdc7c79df9001403e3
yiminwook <grs0416@gmail.com>
2024-10-23 15:47:52 +0900

    Feat: Next AppRouter setup

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -3,7 +3,9 @@ import Button from "@mui/material/Button";
 export default function Header() {
   return (
     <header>
-      <Button variant="contained">Hello world</Button>
+      <Button variant="contained" sx={{ backgroundColor: "red" }}>
+        Hello world
+      </Button>
       header
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Box, BoxProps, styled } from "@mui/material";
+import StyledCustomization from "./_component/Slider";
+
+const Box2 = styled(Box)``;
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <div>home</div>
+      <StyledCustomization />
+    </div>
+  );
 }
