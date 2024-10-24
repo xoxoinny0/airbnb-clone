6ade1bb1a704ffe1b341fc3503cc75294f8f1ab7
xoxoinny0 <xoxoinny0@gmail.com>
2024-10-24 17:25:35 +0900

    Feat : header수정

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -1,7 +1,11 @@
 import { Avatar, Box } from "@mui/material";
-import AirBnbLogo from "/public/assets/airbnb.svg?react";
+// import AirBnbLogo from "/public/assets/airbnb.svg?react";
+import AirBnbLogo from "/public/assets/airbnbtext.svg?react";
+import LanguageLogo from "/public/assets/language.svg?react";
+
 import { faker } from "@faker-js/faker";
 import MenuIcon from "@mui/icons-material/Menu";
+import { SearchTabBtn } from "./Home.style";
 
 const HEADER_HEIGHT = 80;
 
@@ -34,22 +38,48 @@ export default function Header() {
           border: "1px solid red",
         }}
       >
-        <Box>
+        <Box
+          sx={{
+            display: {
+              xs: "none",
+              lg: "block",
+            },
+          }}
+        >
           <AirBnbLogo />
         </Box>
-        <Box>숙소 체험</Box>
-        <Box>
-          <Box
-            component="button"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              borderRadius: 9999,
-              border: "none",
-            }}
-          >
-            <MenuIcon />
-            <Avatar alt="my-avartar" src={faker.image.avatar()} />
+        <Box sx={{}}>
+          <SearchTabBtn className="active">숙소</SearchTabBtn>
+          <SearchTabBtn>체험</SearchTabBtn>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ padding: "13px" }}>당신의 공간을 에어비앤비하세요</Box>
+          <Box sx={{ padding: "13px", cursor: "pointer" }}>
+            <LanguageLogo />
+          </Box>
+          <Box>
+            <Box
+              component="button"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                borderRadius: 9999,
+                border: "none",
+                padding: "13px",
+                cursor: "pointer",
+                ":hover": {
+                  backgroundColor: "rgba(0, 0, 0, 0.1)",
+                },
+              }}
+            >
+              <MenuIcon />
+              <Avatar alt="my-avartar" src={faker.image.avatar()} />
+            </Box>
           </Box>
         </Box>
       </Box>
